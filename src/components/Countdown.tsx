915edec6a0273ddeb9f580ce1abea00ee42444d7
dandankarai <cunhacb22@gmail.com>
2021-02-24 14:01:31 -0300

    Countdown funcionando

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,22 +1,50 @@
+import { useState, useEffect } from 'react'
 import styles from '../styles/components/Countdown.module.css'
 
 export function Countdown() {
+
+  const [time, setTime] = useState(25 * 60)//calculo para trabalhar com segundos
+  const [active, setActive] = useState(false)
+
+  const minutes = Math.floor(time / 60); //minutos arredondados
+  const seconds = time % 60; //resto do que sobra da divisão da linha de cima torna os segundos 
+  
+  //verifico se tem começa com dois, pra separar em duas strings diferentes '2' e '5'
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondsLeft, secondsRight] = String(seconds).padStart(2, '0').split('');
+
+  function startCountdown(){
+    setActive(true);
+  }
+  
+  useEffect(() =>{
+    if(active && time > 0){
+      setTimeout(() => {
+        setTime(time - 1);
+      },1000)
+    }
+  }, [active, time])
+
   return (
 
-    <div className={styles.countdownContainer}>
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
 
-      <div>
-        <span>2</span>
-        <span>5</span>
+        <span>:</span>
+
+        <div>
+          <span>{secondsLeft}</span>
+          <span>{secondsRight}</span>
+        </div>
       </div>
 
-      <span>:</span>
-      
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
-
+      <button type="button" className={styles.countdownButton} onClick={startCountdown}>
+        Iniciar um ciclo
+      </button>
     </div>
 
   )
